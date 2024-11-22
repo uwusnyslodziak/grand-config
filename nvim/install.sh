0d3eb1c164fed7886ff4b4bac622a124f3f22a8f
uwusny <uwusnyslodziak@gmail.com>
2024-11-22 18:50:05 +0100

    added install script for nvim

---
+++ nvim/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+git clone https://github.com/uwusnyslodziak/grand-config.git
+cp grand-config/nvim ~/.config -r
+ 
+
